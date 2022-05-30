a736491791a28841b19396a318da96d2871cfa73
Faisalla <raja_faisallatif@hotmail.com>
2022-05-30 15:17:34 +0100

    New File

---
+++ Hello.java
@@ -0,0 +1,15 @@
+    
+    public class Hello {
+
+    public static void main(String[] args) {
+
+        //This would print Hello World, This is my first repo
+        system.out.println ("Hello World! This is my first repo");
+                    
+        for(int i=1; i<=5; i++){
+                        system.out.println ("Hello World!" +i);
+
+                    }
+                    
+    }
+}
