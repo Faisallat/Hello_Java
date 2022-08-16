3b4b4b4c866af40c74da3e749e24d192c4963c6a
Faisal Latif <105423534+Faisallat@users.noreply.github.com>
2022-08-16 22:16:29 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,14 +1,14 @@
     
-    public class Hello {
+    public class Hello{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-    System.out.println ("Hello World! This is my first repo");
+    System.out.println("Hello World! This is my first repo");
                     
         for(int i=1; i<=10; i++){
-            System.out.println ("This is working file" i);
+            System.out.println("This is working file" +i);
 
-                    }
+        }
                     
     }
 }
