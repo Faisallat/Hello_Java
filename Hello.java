2e975467b9bab8b33c7d3f86a122a9b98f56ab46
Faisal Latif <105423534+Faisallat@users.noreply.github.com>
2022-08-16 22:25:44 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
 
     public static void main(String[] args){
 
-    System.out.println(x: "Hello World! This is my first Git repository");
+    System.out.println("Hello World! This is my first Git repository");
                     
         for(int i=1; i<=5; i++){
             System.out.println("Hello Jenkins World!" +i);
